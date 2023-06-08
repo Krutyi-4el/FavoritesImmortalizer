3617ec85d5d044f17e50c40f92a53126c2f8f998
Krutyi-4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-06-08 16:58:45 +0300

    add service

---
+++ app/src/main/java/ua/k4/immortalizer/ImmortalizerService.kt
@@ -0,0 +1,86 @@
+package ua.k4.immortalizer
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+import kotlin.io.path.Path
+import kotlin.io.path.isExecutable
+import kotlin.io.path.notExists
+
+class ImmortalizerService : Service() {
+    private val pipeFile = Path(applicationContext.filesDir.path, "pipe")
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        thread { setResult(checkAndRun()) }
+    }
+
+    private fun checkAndRun(): String? {
+        val systemPath = System.getenv("PATH")?.split(":") ?: return getString(R.string.no_path)
+        arrayOf("su", "sh", "mkfifo").filter { program ->
+            !systemPath.any { directory ->
+                Path(
+                    directory, program
+                ).isExecutable()
+            }
+        }.let {
+            if (it.isNotEmpty()) return getString(R.string.no_programs) + it.joinToString(prefix = " ")
+        }
+
+        if (pipeFile.notExists()) ProcessBuilder("mkfifo", pipeFile.toString()).start().waitFor()
+            .let {
+                if (it != 0) return getString(R.string.mkfifo_failed)
+            }
+
+        val pipeCheck = ProcessBuilder("sh", "-c", "echo true > '$pipeFile'").start()
+        if (!pipeCheck.waitFor(1, TimeUnit.SECONDS)) {
+            pipeCheck.destroy()
+            val su = ProcessBuilder(
+                "su", "-c", "id -u; while true; do eval \$(cat '$pipeFile' || echo break); done"
+            ).start()
+            if (su.inputStream.bufferedReader().readLine() != "0") {
+                su.destroy()
+                return getString(R.string.no_root)
+            }
+        }
+
+        isRunning = true
+        thread { mainLoop() }
+        return null
+    }
+
+    private fun mainLoop() {
+        while (isRunning) {
+            Thread.sleep(1000)
+            Log.d("ImmortalizerService", "loop")
+        }
+    }
+
+    companion object {
+        var isRunning = false
+        private var result = CompletableFuture<String?>()
+
+        fun getResult(): String? {
+            val res = result.get()
+            result = CompletableFuture()
+            return res
+        }
+
+        private fun setResult(value: String?) {
+            result.complete(value)
+        }
+    }
+}
--- app/src/main/java/ua/k4/immortalizer/MainActivity.kt
+++ app/src/main/java/ua/k4/immortalizer/MainActivity.kt
@@ -1,11 +1,52 @@
 package ua.k4.immortalizer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // notify that it should continue running
+        if (ImmortalizerService.isRunning) startImmortalizer()
+
+        val button = findViewById<Button>(R.id.main_button)
+        button.text =
+            getString(if (ImmortalizerService.isRunning) R.string.main_button_stop else R.string.main_button_start)
+        button.setOnClickListener {
+            if (!ImmortalizerService.isRunning) {
+                startImmortalizer()
+                thread {
+                    val result = ImmortalizerService.getResult()
+                    runOnUiThread {
+                        val textView = findViewById<TextView>(R.id.main_text)
+                        if (result != null) {
+                            stopImmortalizer()
+                            textView.text = result
+                        } else {
+                            button.text = getString(R.string.main_button_stop)
+                            textView.text = getString(R.string.success)
+                        }
+                    }
+                }
+            } else {
+                stopImmortalizer()
+                button.text = getString(R.string.main_button_start)
+            }
+        }
+    }
+
+    private fun startImmortalizer() {
+        startService(Intent(applicationContext, ImmortalizerService::class.java))
+    }
+
+    private fun stopImmortalizer() {
+        ImmortalizerService.isRunning = false
+        stopService(Intent(applicationContext, ImmortalizerService::class.java))
     }
 }
